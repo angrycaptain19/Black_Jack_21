ea22068eca4cf9a3a60724959fb0259564b826f2
German <gerkamano@gmail.com>
2021-02-28 22:22:07 +0300

    Добавлены комментарии к каждому действию

--- main.py
+++ main.py
@@ -7,66 +7,67 @@ from random import shuffle  # Модуль рандом
 class Main:
     # Инициализация класса.
     def __init__(self):
-        self.ma1n = Tk()
-        self.ma1n.title('Black Jack [21]')
-        self.ma1n.iconbitmap('favicon.ico')
-        self.ma1n.geometry('258x421')
-        self.ma1n.resizable(False, False)
-        self.ma1n.attributes('-topmost', True)
-        self.flag = False
-        self.cards = []
-        self.load_images(self.cards)
-        self.deck = list(self.cards)
-        shuffle(self.deck)
-        self.dealer_hand = []
-        self.player_hand = []
+        self.ma1n = Tk()  # Создание окна
+        self.ma1n.title('Black Jack [21]')  # Название программы
+        self.ma1n.iconbitmap('favicon.ico')  # Добавляем иконку
+        self.ma1n.geometry('258x421')  # Определяем размер окна
+        self.ma1n.resizable(False, False)  # Окно нельзя расширять по осям Х и У
+        self.ma1n.attributes('-topmost', True)  # Окно при запуске уходит на передний план
+        self.cards = []  # Определяем список для карт
+        self.load_images(self.cards)  # Вызываем метод загрузки фотографий
+        self.deck = list(self.cards)  # Определяем колоду карт
+        shuffle(self.deck)  # Перемешиваем колоду карт
+        self.dealer_score = 0  # Обозначаем очки дилера
+        self.player_score = 0  # Обозначаем очки игрока
+        self.dealer_hand = []  # Обозначаем "руку" дилера
+        self.player_hand = []  # Обозначаем "руку" игрока
 
-        self.background_image = ImageTk.PhotoImage(Img.open('Background.png'))
-        self.deal_image = ImageTk.PhotoImage(Img.open('Deal.png'))
-        self.hit_image = ImageTk.PhotoImage(Img.open('Hit.png'))
-        self.stand_image = ImageTk.PhotoImage(Img.open('Stand.png'))
-        self.background_label = Label(self.ma1n, image=self.background_image)
-        self.status_label = Label(self.ma1n)
-        self.dealer_score_label = IntVar()
-        self.dealer_label = Label(self.ma1n, textvariable=self.dealer_score_label, bg='#3b3a38', fg='#FFFFFF')
-        self.dealer_card_frame = Frame(self.ma1n, bg='#FFFFFF')
-        self.player_score_label = IntVar()
-        self.player_label = Label(self.ma1n, textvariable=self.player_score_label, bg='#3b3a38', fg='#FFFFFF')
-        self.player_card_frame = Frame(self.ma1n, bg='#FFFFFF')
-        self.button_deal = Button(self.ma1n, image=self.deal_image, relief=FLAT, border='0', command=self.deal)
-        self.button_hit = Button(self.ma1n, image=self.hit_image, relief=FLAT, border='0', command=self.hit)
-        self.button_stand = Button(self.ma1n, image=self.stand_image, relief=FLAT, border='0', command=self.stand)
+        self.background_image = ImageTk.PhotoImage(Img.open('Background.png'))  # Определяем текстуру фона окна
+        self.deal_image = ImageTk.PhotoImage(Img.open('Deal.png'))  # Текстура кнопки "начать"
+        self.hit_image = ImageTk.PhotoImage(Img.open('Hit.png'))  # Текстура кнопки "взять"
+        self.stand_image = ImageTk.PhotoImage(Img.open('Stand.png'))  # Текстура кнопки "оставить"
+        self.background_label = Label(self.ma1n, image=self.background_image)   # Определяем фон окна
+        self.status_label = Label(self.ma1n)  # Создаём Label, где будет отображаться статус игры (победа/проигрыш)
+        self.dealer_score_label = IntVar()  # Переменная Integer типа для очков дилера
+        self.dealer_label = Label(self.ma1n, textvariable=self.dealer_score_label, bg='#3b3a38', fg='#FFFFFF')  # Label для очков дилера
+        self.dealer_card_frame = Frame(self.ma1n, bg='#FFFFFF')  # Рамка для карт дилера
+        self.player_score_label = IntVar()  # Переменная Integer типа для очков игрока
+        self.player_label = Label(self.ma1n, textvariable=self.player_score_label, bg='#3b3a38', fg='#FFFFFF')  # Label для очков игрока
+        self.player_card_frame = Frame(self.ma1n, bg='#FFFFFF')  # Рамка для карт игрока
+        self.button_deal = Button(self.ma1n, image=self.deal_image, relief=FLAT, border='0', command=self.deal)  # Кнопка "Начать"
+        self.button_hit = Button(self.ma1n, image=self.hit_image, relief=FLAT, border='0', command=self.hit)  # Кнопка "Взять"
+        self.button_stand = Button(self.ma1n, image=self.stand_image, relief=FLAT, border='0', command=self.stand)  # Кнопка "Оставить"
 
-        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
-        self.dealer_label.place(x=115, y=88, width=30, height=12)
-        self.dealer_card_frame.place(x=73, y=121)
-        self.player_label.place(x=115, y=224, width=30, height=12)
-        self.player_card_frame.place(x=73, y=255)
-        self.button_deal.place(x=90, y=309)
+        self.background_label.place(x=0, y=0, relwidth=1, relheight=1)  # Выставляем в окне Label с фоном
+        self.dealer_label.place(x=115, y=88, width=30, height=12)  # Выставляем счётчик очков дилера
+        self.dealer_card_frame.place(x=73, y=121)  # Выставляем рамку с картами дилера
+        self.player_label.place(x=115, y=224, width=30, height=12)  # Выставляем счётчик очков игрока
+        self.player_card_frame.place(x=73, y=255)  # Выставляем рамку с картами игрока
+        self.button_deal.place(x=90, y=309)  # Выставляем кнопку "Начать игру"
 
-        self.ma1n.mainloop()
+        self.ma1n.mainloop()  # Запуск главного окна
 
     # Загрузка фотографий.
     @staticmethod
     def load_images(card_images):
-        suits = ['heart', 'club', 'diamond', 'spade']
-        face_cards = ['J', 'Q', 'K']
+        suits = ['heart', 'club', 'diamond', 'spade']  # Масти карт (4шт)
+        face_cards = ['J', 'Q', 'K']  # Карты выше 10 ("валет", "дама", "король")
 
-        for i in suits:
-            for j in range(1, 11):
-                card_images.append((j, ImageTk.PhotoImage(Img.open(f'cards/{str(j)}_{i}.png'))))
-            for card in face_cards:
-                card_images.append((10, ImageTk.PhotoImage(Img.open(f'cards/{str(card)}_{i}.png'))))
+        for i in suits:  # Для каждой карты в колоде
+            for j in range(1, 11):  # Проход по картам с цифрами
+                card_images.append((j, ImageTk.PhotoImage(Img.open(f'cards/{str(j)}_{i}.png'))))  # Открывается карта и добавляется в список
+            for card in face_cards:  # Проход по картам с буквами
+                card_images.append((10, ImageTk.PhotoImage(Img.open(f'cards/{str(card)}_{i}.png'))))  # Открывается карта и добавляется в список
 
     # Вывод фотографий в игре.
     def deal_card(self, frame):
-        next_card = self.deck.pop(0)
-        self.deck.append(next_card)
-        Label(frame, image=next_card[1], relief='flat', bg='#FFFFFF').pack(side='left')
-        return next_card
+        next_card = self.deck.pop(0)  # Карта достаётся из колоды и удаляется оттуда. (0 - карту взяли с верха колоды)
+        self.deck.append(next_card)  # В колоду добавляется эта выпавшая карта
+        Label(frame, image=next_card[1], relief='flat', bg='#FFFFFF').pack(side=LEFT)  # Label с картой добавляется в рамку
+        return next_card  # Результат программы - кортеж с весом карты и картинкой карты
 
-    # Подсчёт тузов в 'руке'.
-    @staticmethod
+    # Подсчёт карт в 'руке'.
+    @staticmethod  # Метод - статический, так как не использует переменные или методы класса, и не ссылается на класс
     def score_hand(hand):
         score = 0
         ace = False
@@ -83,78 +84,79 @@ class Main:
 
     # Начало игры.
     def deal(self):
-        self.status_label.place_forget()
-        self.button_hit.place(x=26, y=309)
-        self.button_stand.place(x=155, y=309)
-        self.button_deal.place_forget()
-        self.dealer_card_frame.destroy()
-        self.dealer_card_frame = Frame(self.ma1n, bg='#FFFFFF')
-        self.dealer_card_frame.place(x=73, y=121)
-        self.player_card_frame.destroy()
-        self.player_card_frame = Frame(self.ma1n, bg='#FFFFFF')
-        self.player_card_frame.place(x=73, y=255)
-        self.status_label['text'] = ''
-        self.dealer_hand = []
-        self.player_hand = []
-        self.player_hand.append(self.deal_card(self.player_card_frame))
-        player_score = self.score_hand(self.player_hand)
-        self.player_score_label.set(player_score)
-        if player_score > 21:
+        self.status_label.place_forget()  # Прячем Label со статусом (поражение/победа)
+        self.button_hit.place(x=26, y=309)  # Ставим кнопку "Взять" на координаты
+        self.button_stand.place(x=155, y=309)  # Ставим кнопку "Оставить" на координаты
+        self.button_deal.place_forget()  # Прячем кнопку "Начать игру"
+        self.dealer_card_frame.destroy()  # Удаляем рамку с картами дилера
+        self.dealer_card_frame = Frame(self.ma1n, bg='#FFFFFF')  # Создаём рамку с картами дилера
+        self.dealer_card_frame.place(x=73, y=121)  # Ставим рамку с картами дилера
+        self.player_card_frame.destroy()  # Удаляем рамку с картами игрока
+        self.player_card_frame = Frame(self.ma1n, bg='#FFFFFF')  # Создаём рамку с картами игрока
+        self.player_card_frame.place(x=73, y=255)  # Ставим рамку с картами игрока
+        self.status_label['text'] = ''  # Стираем текст из Label со статусом игры
+        self.dealer_hand = []  # Опустошаем колоду дилера
+        self.player_hand = []  # Опустошаем колоду игрока
+        self.player_hand.append(self.deal_card(self.player_card_frame))  # Добавляем в колоду игрока случайную карту
+        self.player_score = self.score_hand(self.player_hand)  # Обновляем очки для игрока
+        self.player_score_label.set(self.player_score)  # Обновляем Label, показывающий очки игрока
+        if self.player_score > 21:  # Условие для перебора карт
             self.status_label.place(x=0, y=400)
             self.status_label['text'] = 'Перебор'
-        self.dealer_hand.append(self.deal_card(self.dealer_card_frame))
-        self.dealer_score_label.set(self.score_hand(self.dealer_hand))
-        self.player_hand.append(self.deal_card(self.player_card_frame))
-        player_score = self.score_hand(self.player_hand)
-        self.player_score_label.set(player_score)
-        if player_score > 21:
+        self.dealer_hand.append(self.deal_card(self.dealer_card_frame))  # Добавляем в колоду дилера случайную карту
+        self.dealer_score = self.score_hand(self.dealer_hand)   # Обновляем очки для дилера
+        self.dealer_score_label.set(self.dealer_score)  # Обновляем Label, показывающий очки игрока
+        self.player_hand.append(self.deal_card(self.player_card_frame))  # Добавляем в колоду дилера случайную карту
+        self.player_score = self.score_hand(self.player_hand)  # Обновляем очки для дилера
+        self.player_score_label.set(self.player_score)  # Обновляем Label, показывающий очки игрока
+        if self.player_score > 21:  # Условие для перебора карт
             self.status_label.place(x=0, y=400)
             self.status_label['text'] = 'Перебор'
-        if player_score == 21:
+        if self.player_score == 21:  # Условие для Блекджека (если в сумме карты дают 21)
             self.stand()
 
     # Кнопка 'Взять'.
     def hit(self):
-        self.player_hand.append(self.deal_card(self.player_card_frame))
-        player_score = self.score_hand(self.player_hand)
-        self.player_score_label.set(player_score)
-        if player_score > 21:
+        self.player_hand.append(self.deal_card(self.player_card_frame))  # Добавляем в колоду дилера случайную карту
+        self.player_score = self.score_hand(self.player_hand)  # Обновляем очки для игрока
+        self.player_score_label.set(self.player_score)  # Обновляем Label, показывающий очки игрока
+        if self.player_score > 21:  # Условие для перебора карт
             self.status_label.place(x=0, y=400)
             self.status_label['text'] = 'Перебор'
             self.button_hit.place_forget()
             self.button_stand.place_forget()
             self.button_deal.place(x=90, y=309)
-        if player_score == 21:
+        if self.player_score == 21:  # Условие для Блекджека (если в сумме карты дают 21)
             self.stand()
 
     # Кнопка 'Пас'.
     def stand(self):
-        dealer_score = self.score_hand(self.dealer_hand)
-        while 0 < dealer_score < 17:
-            self.dealer_hand.append(self.deal_card(self.dealer_card_frame))
-            dealer_score = self.score_hand(self.dealer_hand)
-            self.dealer_score_label.set(dealer_score)
-        player_score = self.score_hand(self.player_hand)
-        if player_score > 21:
+        self.dealer_score = self.score_hand(self.dealer_hand)
+        while 0 < self.dealer_score < 17:  # Дилер берёт карты, в сумме до 17и
+            self.dealer_hand.append(self.deal_card(self.dealer_card_frame))  # Добавляем в колоду дилера случайную карту
+            self.dealer_score = self.score_hand(self.dealer_hand)  # Обновляем очки для дилера
+            self.dealer_score_label.set(self.dealer_score)  # Обновляем Label, показывающий очки дилера
+        self.player_score = self.score_hand(self.player_hand)  # Обновляем очки для игрока
+        if self.player_score > 21:  # Условие для перебора
             self.status_label.place(x=0, y=400)
             self.status_label['text'] = 'Перебор'
-        elif dealer_score > 21 or dealer_score < player_score:
+        elif self.dealer_score > 21 or self.dealer_score < self.player_score:  # Условие для победы
             self.status_label.place(x=0, y=400)
-            if player_score == 21:
+            if self.player_score == 21:  # Дополнительное условие для Блекджека (если в сумме карты дают 21)
                 self.status_label['text'] = 'У вас 21!'
             else:
                 self.status_label['text'] = 'Вы выиграли!'
-        elif dealer_score > player_score:
+        elif self.dealer_score > self.player_score:  # Условие для Блекджека (если в сумме карты дают 21)
             self.status_label.place(x=0, y=400)
             self.status_label['text'] = 'Вы проиграли'
-        else:
+        else:  # Иначе условие для ничьей
             self.status_label.place(x=0, y=400)
             self.status_label['text'] = 'Ничья'
-        self.button_hit.place_forget()
-        self.button_stand.place_forget()
-        self.button_deal.place(x=90, y=309)
+        self.button_hit.place_forget()  # Прячем кнопку "Взять"
+        self.button_stand.place_forget()  # Прячем кнопку "Оставить"
+        self.button_deal.place(x=90, y=309)  # Показываем кнопку "Начать игру"
 
 
 # Старт игры.
 if __name__ == '__main__':
-    _new_window = Main()
+    _new_window = Main()  # Создаём переменную и присваиваем ей класс
